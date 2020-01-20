26c262e3f0d0851efcc490fec0c82ace766abfcf
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-01-20 23:52:23 +0200

    added v2 for FzBz

--- scripts/fzbz.js
+++ scripts/fzbz.js
@@ -1,19 +1,36 @@
 console.log("started Fizz Buzz");
 //====v1 =============================================
-const fizzBuzz = num => {
-    for(let i = 1; i <= num; i++) {
-      // check if the number is a multiple of 3 and 5
-      if(i % 3 === 0 && i % 5 === 0) {
-        console.log('fizzbuzz')
-      } // check if the number is a multiple of 3
-        else if(i % 3 === 0) {
-        console.log('fizz')
-      } // check if the number is a multiple of 5
-        else if(i % 5 === 0) {
-        console.log('buzz')
-      } else {
-        console.log(i)
-      }
-    }
-  }
-  //====================================================
+// const fizzBuzz = num => {
+//     for(let i = 1; i <= num; i++) {
+//       // check if the number is a multiple of 3 and 5
+//       if(i % 3 === 0 && i % 5 === 0) {
+//         console.log('fizzbuzz')
+//       } // check if the number is a multiple of 3
+//         else if(i % 3 === 0) {
+//         console.log('fizz')
+//       } // check if the number is a multiple of 5
+//         else if(i % 5 === 0) {
+//         console.log('buzz')
+//       } else {
+//         console.log(i)
+//       }
+//     }
+//   }
+  //====v2================================================
+  for (var i=1; i <= 20; i++)
+{
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
+}
+//====v================================================
+//====v================================================
+//====v================================================
+//====v================================================
+//====v================================================
+//====v================================================
