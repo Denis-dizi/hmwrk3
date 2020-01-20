447c587e7d97beb15a2d3af99fa417fcf72c0982
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-01-20 23:56:36 +0200

    added v3

--- scripts/fzbz.js
+++ scripts/fzbz.js
@@ -16,19 +16,30 @@ console.log("started Fizz Buzz");
 //       }
 //     }
 //   }
-  //====v2================================================
-  for (var i=1; i <= 20; i++)
-{
-    if (i % 15 == 0)
-        console.log("FizzBuzz");
-    else if (i % 3 == 0)
-        console.log("Fizz");
-    else if (i % 5 == 0)
-        console.log("Buzz");
-    else
-        console.log(i);
-}
-//====v================================================
+//====v2================================================
+//   for (var i=1; i <= 20; i++)
+// {
+//     if (i % 15 == 0)
+//         console.log("FizzBuzz");
+//     else if (i % 3 == 0)
+//         console.log("Fizz");
+//     else if (i % 5 == 0)
+//         console.log("Buzz");
+//     else
+//         console.log(i);
+// }
+//====v3================================================
+for (var i=1; i<=20; i++)(function(i){
+    var output = ""
+  
+    if (i % 3 == 0)
+     output += "Fizz"
+  
+    if (i % 5 == 0)
+     output += "Buzz"
+  
+    console.log(output || i)
+  })(i)
 //====v================================================
 //====v================================================
 //====v================================================
