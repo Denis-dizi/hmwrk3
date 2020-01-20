14c4dbb3eb7d3b124b5976eee3467f3cd9e169b2
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-01-21 00:17:56 +0200

    added some more variations

--- scripts/fzbz.js
+++ scripts/fzbz.js
@@ -1,21 +1,22 @@
 console.log("started Fizz Buzz");
 //====v1 =============================================
-// const fizzBuzz = num => {
-//     for(let i = 1; i <= num; i++) {
-//       // check if the number is a multiple of 3 and 5
-//       if(i % 3 === 0 && i % 5 === 0) {
-//         console.log('fizzbuzz')
-//       } // check if the number is a multiple of 3
-//         else if(i % 3 === 0) {
-//         console.log('fizz')
-//       } // check if the number is a multiple of 5
-//         else if(i % 5 === 0) {
-//         console.log('buzz')
-//       } else {
-//         console.log(i)
-//       }
-//     }
-//   }
+const fizzBuzz = num => {
+    for(let i = 1; i <= num; i++) {
+      // check if the number is a multiple of 3 and 5
+      if(i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+      } // check if the number is a multiple of 3
+        else if(i % 3 === 0) {
+        console.log('fizz')
+      } // check if the number is a multiple of 5
+        else if(i % 5 === 0) {
+        console.log('buzz')
+      } else {
+        console.log(i)
+      }
+    }
+  }
+  fizzBuzz(20);
 //====v2================================================
 //   for (var i=1; i <= 20; i++)
 // {
@@ -29,19 +30,21 @@ console.log("started Fizz Buzz");
 //         console.log(i);
 // }
 //====v3================================================
-for (var i=1; i<=20; i++)(function(i){
-    var output = ""
-  
-    if (i % 3 == 0)
-     output += "Fizz"
-  
-    if (i % 5 == 0)
-     output += "Buzz"
-  
-    console.log(output || i)
-  })(i)
-//====v================================================
-//====v================================================
+// for (var i=1; i<=20; i++)(function(i){
+//     var output = ""
+//     if (i % 3 == 0)
+//      output += "Fizz"
+//     if (i % 5 == 0)
+//      output += "Buzz"
+//     console.log(output || i)
+//   })(i)
+//====v4================================================
+// for (var i = 1; i <= 20; i++) {
+//     var f = i % 3 == 0, b = i % 5 == 0;
+//     console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);
+//   }
+//====v5================================================
+// for(i=0;i<20;)console.log((++i%3?'':'Fizz')+(i%5?'':'Buzz')||i)
 //====v================================================
 //====v================================================
 //====v================================================
